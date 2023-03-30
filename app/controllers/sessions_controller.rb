a9d0ca1033da5df18dade4e715fdcbd839115491
tony555t <tonytullah764@gmail.com>
2023-03-30 12:28:51 +0300

    pending error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
-end
+    def create
+      user = User.find_by(username: params[:username])
+  
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user
+      else
+        render json: { error: 'Invalid username or password' }, status: :unauthorized
+      end
+    end
+  
+    def destroy
+      session.delete(:user_id)
+      head :no_content
+    end
+  end
+  
